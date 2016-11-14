eb562e8c36c0a92a9c763a72c286d443c4397237
solmaro <200910766@sangmyung.kr>
2016-11-14 12:15:59 +0900

    Add files via upload

---
+++ ds_rest_p1.py
@@ -0,0 +1,28 @@
+﻿import os
+KEY="694f495468646b73313130676c426659"
+TYPE='json'
+SERVICE='SearchSTNBySubwayLineService'
+START_INDEX='1'
+END_INDEX='10'
+LINE_NUM='2'
+
+url='http://openapi.seoul.go.kr:8088/'
+url+=KEY
+url+='/'
+url+=TYPE
+url+='/'
+url+=SERVICE
+url+='/'
+url+=START_INDEX
+url+='/'
+url+=END_INDEX
+url+='/'
+url+=LINE_NUM
+	
+
+print url
+
+import requests
+data=requests.get(url)
+
+print data.text
